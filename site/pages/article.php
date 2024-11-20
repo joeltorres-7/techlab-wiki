e919885210839be9b8d5084b5efc9c39c331d81f
Joel Torres <thisisjoeltorres@gmail.com>
2024-11-20 16:31:00 -0500

    MVP-002 - Adds missing viewport meta tag.

--- site/pages/article.php
+++ site/pages/article.php
@@ -35,6 +35,7 @@ if (file_exists($filePath)) {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
@@ -44,7 +45,6 @@ if (file_exists($filePath)) {
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="./resources/styles/styles.css">
     <title>TechLab - <?= htmlspecialchars($customTitle) ?></title>
-    <meta charset="UTF-8">
 </head>
 
 <body>
