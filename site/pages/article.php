2646bc454dcf3bbeb05a5d623b8efca9ffa40587
Joel Torres <thisisjoeltorres@gmail.com>
2024-11-21 16:33:35 -0500

    MVP-003 - Adds favicon to all pages.

--- site/pages/article.php
+++ site/pages/article.php
@@ -48,6 +48,7 @@ if (file_exists($filePath)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./resources/images/icons/favicon.ico" type="image/x-icon"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
